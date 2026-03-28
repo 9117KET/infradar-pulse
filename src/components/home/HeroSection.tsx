1b9da702f40296f1e19e671728ab7c2db3ad43f1
Kinlo Ephriam Tangiri (KET) <kinlotangiri911@gmail.com>
2026-03-28 11:12:00 +0000

    Preceding changes

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion';
 import { Button } from '@/components/ui/button';
-import { HeroMap } from '@/components/home/HeroMap';
+import { HeroLiveTracker } from '@/components/home/HeroLiveTracker';
 import { useProjects } from '@/hooks/use-projects';
 
 export function HeroSection() {
