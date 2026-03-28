593e39be23f99c18278beb876b7e9c195d6a39c5
Kinlo Ephriam Tangiri (KET) <kinlotangiri911@gmail.com>
2026-03-28 12:21:15 +0000

    Preceding changes

--- src/App.tsx
+++ src/App.tsx
@@ -59,7 +59,7 @@ const App = () => (
               <Route path="/pricing" element={<Pricing />} />
               <Route path="/about" element={<About />} />
               <Route path="/contact" element={<Contact />} />
-              <Route path="/waitlist" element={<Waitlist />} />
+              
               <Route path="/terms" element={<Terms />} />
               <Route path="/privacy" element={<Privacy />} />
               <Route path="/data-protection" element={<DataProtection />} />
